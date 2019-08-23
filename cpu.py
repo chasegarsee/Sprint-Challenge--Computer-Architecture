b73848b7b440ec7e04e0e4628cda0895697d2570
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-08-23 10:54:59 -0500

    added arithmetic logic unit

--- cpu.py
+++ cpu.py
@@ -44,3 +44,40 @@ class CPU:
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {file_name} not found")
             sys.exit(2)
+
+    def alu(self, op, register_a, register_b):
+        if op == "ADD":
+            self.registers[register_a] += self.registers[register_b]
+        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.registers[register_a] = self.registers[register_a] * \
+                self.registers[register_b]
+        elif op == "CMP":
+            if self.registers[register_a] == self.registers[register_b]:
+                self.flag = 0b00000001
+            elif self.registers[register_a] > self.registers[register_b]:
+                self.flag = 0b00000010
+            else:
+                self.flag = 0b00000100
+        else:
+            raise Exception("Unsupported ALU operation")
+
+    def trace(self):
+        """
+        Handy function to print out the CPU state. You might want to call this
+        from run() if you need help debugging.
+        """
+
+        print(f"TRACE: %02X | %02X %02X %02X |" % (
+            self.pc,
+            # self.fl,
+            # self.ie,
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
+        ), end='')
+
+        for i in range(8):
+            print(" %02X" % self.reg[i], end='')
+
+        print()
